4a1bd07656cf89b1bf8df1f68a294a26f6f2c046
itsamittomar <amitsingh10d@gmail.com>
2020-06-19 09:13:25 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+#py -c "import os; print(os.urandom(16))"
+
+class Config(object):
+    SECRET_KEY=os.environ.get("SECRET_KEY") or b'\x1d\xe6\xf23=\xebr\x9a\xf17\xb8<4\x08\x9cf'
+
+    MONGODB_SETTINGS = { 'db' : 'TCS_Project'}
